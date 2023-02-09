bdeabc6da7845b0f0426e9be6d7ee34174ffedd0
Gaikawade <gmhs6199@gmail.com>
2023-02-09 18:00:31 +0530

    delete comment

--- blog/routes.py
+++ blog/routes.py
@@ -123,6 +123,7 @@ def delete_post(post_id):
     flash('The article has been deleted', 'success')
     return redirect(url_for('home'))
 
+
 @app.route('/add_comment/<int:post_id>', methods=['POST', 'GET'])
 @login_required
 def add_comment(post_id):
@@ -139,6 +140,21 @@ def add_comment(post_id):
         else:
             flash('Post does not exist', 'error')
     return redirect(url_for('read_post', post_id=post_id))
+
+
+@app.route('/delete_comment/<int:comment_id>', methods=['GET'])
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+    
+    if not comment:
+        flash('Comment does not exist', 'danger')
+    elif current_user.id == comment.author and current_user.id == comment.post.author:
+        flash('You are not allowed to delete this comment', 'danger')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+    return redirect(url_for('read_post', post_id=comment.post_id))
 
 
 @app.route('/like_post/<int:post_id>', methods=['POST'])
--- blog/static/script.js
+++ blog/static/script.js
@@ -1,17 +1,17 @@
-function like(postId){
-  const likeCount = document.getElementById(`likes-count-${postId}`);
-  const likeButton = document.getElementById(`like-button-${postId}`);
+function like(postId) {
+    const likeCount = document.getElementById(`likes-count-${postId}`);
+    const likeButton = document.getElementById(`like-button-${postId}`);
 
-  fetch(`/like_post/${postId}`, {method: 'POST'})
-  .then((res) => res.json())
-  .then((data) =>{
-    console.log(data);
-    likeCount.innerHTML = data[`likes`];
-    if(data[`likes`] == true){
-      likeButton.className = 'fas fa-thumbs-up';
-    } else {
-      likeButton.className = 'far fa-thumbs-up';
-    }
-  })
-  .catch(e => alert('Please try after some time.'));
+    fetch(`/like_post/${postId}`, { method: "POST" })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            likeCount.innerHTML = data[`likes`];
+            if (data[`likes`] == true) {
+                likeButton.className = "fas fa-thumbs-up";
+            } else {
+                likeButton.className = "far fa-thumbs-up";
+            }
+        })
+        .catch((e) => alert("Please try after some time."));
 }
