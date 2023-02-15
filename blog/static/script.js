7d08250d6f8a6aa8974046251d8d541567c8cf4f
Gaikawade <gmhs6199@gmail.com>
2023-02-15 16:29:14 +0530

    added event listener in search page

--- blog/static/script.js
+++ blog/static/script.js
@@ -1,3 +1,5 @@
+const currentURL = document.URL;
+
 function like(postId) {
     const likeCount = document.getElementById(`likes-count-${postId}`);
     const likeButton = document.getElementById(`like-button-${postId}`);
@@ -16,24 +18,53 @@ function like(postId) {
         .catch((e) => alert("Please try after some time."));
 }
 
+if (currentURL.includes("post")) {
+    const expandcommentInput = document.querySelector("#expand-comment-input");
+    const commentField = document.querySelector("#give-comments");
 
-const comments = document.querySelector("#comments");
-const expandComments = document.querySelector("#expand-comments");
+    expandcommentInput.addEventListener("click", () => {
+        if (commentField.style.display == "block") {
+            commentField.style.display = "none";
+        } else {
+            commentField.style.display = "block";
+        }
+    });
 
-expandComments.addEventListener("click", () =>{
-    if (comments.style.display == 'block'){
-        comments.style.display = 'none';
-    } else {
-        comments.style.display = 'block';
-    }
-})
+    const comments = document.querySelector("#comments");
+    const expandComments = document.querySelector("#expand-comments");
 
-const expandcommentInput = document.querySelector('#expand-comment-input');
-const commentField = document.querySelector('#give-comments');
-expandcommentInput.addEventListener('click', () =>{
-    if(commentField.style.display == 'block'){
-        commentField.style.display = 'none';
-    } else {
-        commentField.style.display = 'block';
-    }
-})
+    expandComments.addEventListener("click", () => {
+        if (comments.style.display == "block") {
+            comments.style.display = "none";
+        } else {
+            comments.style.display = "block";
+        }
+    });
+}
+
+if (currentURL.includes("search")) {
+    const posts = document.querySelector("#posts");
+    const expandPosts = document.querySelector("#expand-posts");
+    const users = document.querySelector("#users");
+    const expandUsers = document.querySelector("#expand-users");
+
+    expandPosts.addEventListener("click", () => {
+        if (posts.style.display === "none") {
+            posts.style.display = "block";
+            users.style.display = "none";
+        } else {
+            posts.style.display = "none";
+            users.style.display = "block";
+        }
+    });
+
+    expandUsers.addEventListener("click", () => {
+        if (users.style.display === "none") {
+            users.style.display = "block";
+            posts.style.display = "none";
+        } else {
+            users.style.display = "none";
+            posts.style.display = "block";
+        }
+    });
+}
