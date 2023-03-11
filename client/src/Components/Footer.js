c5d7dd0a216b162eb454a47a43e7262cfb0858d0
Gaikawade <gmhs6199@gmail.com>
2023-03-11 15:55:45 +0530

    babel and webpack config

--- client/src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='small text-center text-muted fst-italic m-5'>
-        Copyright &copy; Blog Website 2023
-    </div>
-  )
-}
